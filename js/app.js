44297554fe6e467ca6aa83813d851b71719a03eb
Dami <obayanjud@gmail.com>
2018-09-15 10:26:13 -0600

    Render enemy sprite on screen

--- js/app.js
+++ js/app.js
@@ -1,19 +1,30 @@
 class Enemy {
-  constructor() {}
+  constructor() {
+    // The image/sprite for our enemies, this uses
+    // a helper to easily load images
+    this.sprite = "images/enemy-bug.png";
+    this.x = 0;
+    this.y = 0;
+    this.speed = 10;
+  }
 
-  render() {}
+  // Draw the enemy on the screen, required method for game
+  render() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
 
-  update() {}
+  updateLocation(x, y) {
+    this.x = x;
+    this.y = y;
+  }
 }
-// Enemies our player must avoid
-var Enemy = function() {
-  // Variables applied to each of our instances go here,
-  // we've provided one for you to get started
 
-  // The image/sprite for our enemies, this uses
-  // a helper we've provided to easily load images
-  this.sprite = "images/enemy-bug.png";
-};
+// the images should have loaded and cached by Resources.load before
+// calling the render method
+window.addEventListener("load", () => {
+  let enemy = new Enemy();
+  enemy.render();
+});
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -21,11 +32,6 @@ Enemy.prototype.update = function(dt) {
   // You should multiply any movement by the dt parameter
   // which will ensure the game runs at the same speed for
   // all computers.
-};
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Now write your own player class
